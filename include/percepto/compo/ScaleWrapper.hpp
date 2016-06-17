6e77570397a2cf4cdcdbdce14dfb1e1b2ceef5ba
Humphrey Hu <humhu@cmu.edu>
2016-06-17 19:38:17 -0400

    Added wrapper to multiply by a scalar.

---
+++ include/percepto/compo/ScaleWrapper.hpp
@@ -0,0 +1,58 @@
+#pragma once
+
+#include "percepto/compo/Interfaces.h"
+
+namespace percepto
+{
+
+template <typename DataType>
+class ScaleWrapper
+: public Source<DataType>
+{
+public:
+
+	typedef Source<DataType> SourceType;
+	typedef Sink<DataType> SinkType;
+
+	ScaleWrapper()
+	: _input( this ) {}
+
+	ScaleWrapper( const ScaleWrapper& other )
+	: _input( this ) {}
+
+	void SetSource( SourceType* s )
+	{
+		s->RegisterConsumer( &_input );
+	}
+
+	void SetScale( double w )
+	{
+		_scale = w;
+	}
+
+	virtual void Foreprop()
+	{
+		SourceType::SetOutput( _input.GetInput() * _scale );
+		SourceType::Foreprop();
+	}
+
+	virtual void BackpropImplementation( const MatrixType& nextDodx )
+	{
+		if( nextDodx.size() == 0 )
+		{
+			// TODO somehow...
+		}
+		else
+		{
+			_input.Backprop( nextDodx * _scale );
+		}
+	}
+
+private:
+
+	double _scale;
+	SinkType _input;
+
+};
+
+}
