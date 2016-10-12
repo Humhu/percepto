4d49292232e243fa7888095146aa3f67e1c3ba2a
Humphrey Hu <humhu@cmu.edu>
2016-10-12 11:51:43 -0400

    Various bugfixes and tweakable parameters added to bandits.

--- bandito/python/bandito/bandits.py
+++ bandito/python/bandito/bandits.py
@@ -3,7 +3,6 @@
 import collections, math
 import numpy as np
 
-
 class BanditInterface:
     """Interface to a generic bandit algorithm.
 
@@ -57,7 +56,7 @@ class BanditInterface:
             raise ValueError( 'Number of arms must equal initial rewards' )
 
         if len(init_rewards) == 0:
-            self.histories = [[[] for i in range(num_arms)]]
+            self.histories = [[] for i in range(num_arms)]
         else:
             if isinstance( init_rewards[0], collections.Iterable ):
                 self.histories = init_rewards
@@ -65,14 +64,34 @@ class BanditInterface:
                 self.histories = [ [r] for r in init_rewards ]
 
         self.selection_func = selection_func
+        self.round_number = 0
 
-    def ask( self, eps ):
+        # TODO Compute round number from init_rewards?
+
+    def ask( self, eps=None ):
         """Request the next arm index to pull.
 
         Must specify the exploration parameter eps.
         """
+        # First we must make sure every arm is pulled at least once
+        for arm,hist in enumerate(self.histories):
+            if len(hist) == 0:
+                print( 'Arm %d does not have any pulls yet.' % arm )
+                return arm
+
+        self.round_number += 1
+        if eps is None:
+            eps = math.log( math.log( self.round_number + 3 ) )
 
         criteria = [ self.selection_func( hist, eps ) for hist in self.histories ]
+
+        print( 'Round %d exploration factor: %f' % (self.round_number, eps) )
+        for arm,crit in enumerate(criteria):
+            mean = np.mean( self.histories[arm] )
+            std = np.std( self.histories[arm] )
+            pulls= len( self.histories[arm] )
+            print( 'Arm %d criteria: %f pulls: %d mean: %f std: %f' % (arm, crit, pulls, mean, std) )
+
         comp = lambda x : x[1]
         largest = max( enumerate(criteria), key=comp )
         return largest[0]
--- bandito/python/bandito/criterions.py
+++ bandito/python/bandito/criterions.py
@@ -9,7 +9,7 @@ ucb_v_criterion
 import collections, math
 import numpy as np
 
-def ucbv_criterion( history, exp_factor ):
+def ucbv_criterion( history, exp_factor, reward_scale=1.0, c=1.0 ):
     """Computes the Upper Confidence Bound Variance (UCB-V) criterion from a 
     history of rewards.
 
@@ -26,13 +26,22 @@ def ucbv_criterion( history, exp_factor ):
         Iterable of previous rewards. Must not be empty.
     exp_factor: numeric
         Non-negative exploration factor.
+    reward_scale: numeric (default 1.0)
+        The size of the reward domain, ie. rewards on [0, b] have size b
+    exp_: numeric default (1.0)
+        Positive tuning constant that adjusts 
     """
     if len(history) == 0:
         raise ValueError( 'History cannot be empty.' )
     if exp_factor < 0.0:
         raise ValueError( 'Exploration factor must be non-negative.' )
+    if reward_scale <= 0.0:
+        raise ValueError( 'Reward scale must be positive.' )
+    if c <= 0.0:
+        raise ValueError( 'Tuning factor c must be positive.' )
 
     emp_mean = np.mean( history )
     emp_var = np.var( history )
     s = len( history )
-    return emp_mean + math.sqrt( 2 * emp_var * exp_factor / s ) + 3.0 * exp_factor / s
+    return emp_mean + math.sqrt( 2 * emp_var * exp_factor / s ) + \
+           c * 3.0 * reward_scale * exp_factor / s
