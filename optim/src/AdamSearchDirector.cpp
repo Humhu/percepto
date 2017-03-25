f413e686cd7a97bc61561fa06aab438848359ed8
Humphrey Hu <humhu@cmu.edu>
2017-03-25 12:47:51 -0400

    Refactor/structure of optim

--- optim/src/AdamSearchDirector.cpp
+++ optim/src/AdamSearchDirector.cpp
@@ -6,51 +6,51 @@ using namespace argus;
 namespace percepto
 {
 
-AdamSearchDirector::AdamSearchDirector() 
-: _beta1( 0.9 ), _beta2( 0.999 ), _epsilon( 1E-7 ) 
+AdamSearchDirector::AdamSearchDirector()
+	: _beta1(0.9), _beta2(0.999), _epsilon(1E-7)
 {
 	Reset();
 }
 
-template <typename InfoType>
-void AdamSearchDirector::InitializeFromInfo( const InfoType& info )
+template<typename InfoType>
+void AdamSearchDirector::InitializeFromInfo(const InfoType& info)
 {
 	double b1, b2, eps;
-	if( GetParam( info, "beta1", b1 ) )
+	if(GetParam(info, "beta1", b1))
 	{
-		SetBeta1( b1 );
+		SetBeta1(b1);
 	}
-	if( GetParam( info, "beta2", b2 ) )
+	if(GetParam(info, "beta2", b2))
 	{
-		SetBeta2( b2 );
+		SetBeta2(b2);
 	}
-	if( GetParam( info, "epsilon", eps ) )
+	if(GetParam(info, "epsilon", eps))
 	{
-		SetEpsilon( eps );
+		SetEpsilon(eps);
 	}
 }
 
-void AdamSearchDirector::Initialize( const ros::NodeHandle& ph )
+void AdamSearchDirector::Initialize(const ros::NodeHandle& ph)
 {
-	InitializeFromInfo( ph );
+	InitializeFromInfo(ph);
 }
 
-void AdamSearchDirector::Initialize( const YAML::Node& node )
+void AdamSearchDirector::Initialize(const YAML::Node& node)
 {
-	InitializeFromInfo( node );
+	InitializeFromInfo(node);
 }
 
-void AdamSearchDirector::SetBeta1( double b1 )
+void AdamSearchDirector::SetBeta1(double b1)
 {
 	_beta1 = b1;
 }
 
-void AdamSearchDirector::SetBeta2( double b2 )
+void AdamSearchDirector::SetBeta2(double b2)
 {
 	_beta2 = b2;
 }
 
-void AdamSearchDirector::SetEpsilon( double eps )
+void AdamSearchDirector::SetEpsilon(double eps)
 {
 	_epsilon = eps;
 }
@@ -62,34 +62,34 @@ void AdamSearchDirector::Reset()
 	_m = VectorType();
 }
 
-VectorType AdamSearchDirector::ComputeSearchDirection( OptimizationProblem& problem )
+VectorType AdamSearchDirector::ComputeSearchDirection(OptimizationProblem& problem)
 {
 	VectorType gradient = problem.ComputeGradient();
 
 	// Need to initialize state
-	if( _m.size() == 0 )
+	if(_m.size() == 0)
 	{
-		_m = VectorType::Zero( gradient.size() );
-		_v = VectorType::Zero( gradient.size() );
+		_m = VectorType::Zero(gradient.size());
+		_v = VectorType::Zero(gradient.size());
 	}
 
-	if( gradient.size() != _m.size() )
+	if(gradient.size() != _m.size())
 	{
-		throw std::runtime_error( "Gradient dimension mismatch." );
+		throw std::runtime_error("Gradient dimension mismatch.");
 	}
 
-	_m = _beta1 * _m + (1.0 - _beta1 ) * gradient;
-	VectorType gradientSq = ( gradient.array() * gradient.array() ).matrix();
-	_v = _beta2 * _v + (1.0 - _beta2 ) * gradientSq;
+	_m = _beta1 * _m + (1.0 - _beta1) * gradient;
+	VectorType gradientSq = (gradient.array() * gradient.array()).matrix();
+	_v = _beta2 * _v + (1.0 - _beta2) * gradientSq;
 
-	VectorType mhat = _m / ( 1.0 - _beta1Acc );
-	VectorType vhat = _v / ( 1.0 - _beta2Acc );
+	VectorType mhat = _m / (1.0 - _beta1Acc);
+	VectorType vhat = _v / (1.0 - _beta2Acc);
 	_beta1Acc *= _beta1;
 	_beta2Acc *= _beta2;
 
-	VectorType ret = ( mhat.array() / ( vhat.array().sqrt() + _epsilon ) ).matrix();
+	VectorType ret = (mhat.array() / (vhat.array().sqrt() + _epsilon)).matrix();
 
-	if( problem.IsMinimization() )
+	if(problem.IsMinimization())
 	{
 		return -ret;
 	}
