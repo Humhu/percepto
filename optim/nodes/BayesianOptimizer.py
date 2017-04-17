ca1a77e56aa4cc9935c5f3e55b2eaba7c3b89134
Humphrey Hu <humhu@cmu.edu>
2017-04-17 17:55:19 -0400

    Fixed various bugs, refactored optim

--- optim/nodes/BayesianOptimizer.py
+++ optim/nodes/BayesianOptimizer.py
@@ -3,11 +3,8 @@
 from itertools import izip
 import numpy as np
 import rospy
-import optim.reward_models as orm
-import optim.optimization as opt
-import optim.arm_selectors as oas
-import optim.ros_interface as opt_ros
-
+import optim
+import matplotlib.pyplot as plt
 
 class BayesianOptimizer(object):
     """Iteratively samples and updates a reward model (response surface) to optimize a function.
@@ -19,11 +16,11 @@ class BayesianOptimizer(object):
 
         # Parse reward model
         model_info = rospy.get_param('~reward_model')
-        self.reward_model = orm.parse_reward_model(model_info)
+        self.reward_model = optim.parse_reward_model(model_info)
 
         # Parse acquisition optimizer
         optimizer_info = rospy.get_param('~auxiliary_optimizer')
-        self.aux_optimizer = opt.parse_optimizers(optimizer_info)
+        self.aux_optimizer = optim.parse_optimizers(optimizer_info)
 
         def farr(x, n):
             if hasattr(x, '__iter__'):
@@ -34,23 +31,31 @@ class BayesianOptimizer(object):
         dim = rospy.get_param('~dim')
         self.lower_bounds = farr(rospy.get_param('~lower_bounds'), dim)
         self.upper_bounds = farr(rospy.get_param('~upper_bounds'), dim)
-        self.aux_optimizer.opts['bounds'] = [
-            self.lower_bounds, self.upper_bounds]
+        self.aux_optimizer.lower_bounds = self.lower_bounds
+        self.aux_optimizer.upper_bounds = self.upper_bounds
         self.aux_x_init = 0.5 * (self.lower_bounds + self.upper_bounds)
         self.aux_x_init[np.logical_not(np.isfinite(self.aux_x_init))] = 0
         #self.aux_x_init = np.zeros(dim)
 
+        self.visualize = rospy.get_param('~visualize')
+        if self.visualize:
+            if dim != 1:
+                rospy.logwarn('Visualization is only enabled for 1D reward model!')
+                self.visualize = False
+            else:
+                plt.ion()
+
         # TODO Parse selection approach
         # TODO Support partial optimization
-        self.acq_func = oas.UCBAcquisition(self.reward_model)
+        self.acq_func = optim.UCBAcquisition(self.reward_model)
 
         self.rounds = []
         init_info = rospy.get_param('~initialization')
         self.num_init = init_info['num_samples']
         init_method = init_info['method']
         if init_method == 'uniform':
-            self.init_sample_func = lambda: np.random.uniform(
-                self.lower_bounds, self.upper_bounds)
+            self.init_sample_func = lambda: np.random.uniform(self.lower_bounds,
+                                                              self.upper_bounds)
         # TODO Support for latin hypercubes, other approaches?
         else:
             raise ValueError(
@@ -64,13 +69,13 @@ class BayesianOptimizer(object):
         """Selects the next sample for initialization by sampling from
         a random distribution.
         """
-        return self.init_sample_func()
+        return np.atleast_1d(self.init_sample_func())
 
     def pick_next_sample(self):
         """Selects the next sample to explore by optimizing the acquisition function.
         """
-        x, acq = self.aux_optimizer.optimize(
-            x0=self.aux_x_init, func=self.acq_func)
+        x, acq = self.aux_optimizer.optimize(x_init=self.aux_x_init,
+                                             func=self.acq_func)
         rospy.loginfo('Next sample %s with acquisition value %f', str(x), acq)
         return x
 
@@ -79,6 +84,20 @@ class BayesianOptimizer(object):
         """
         # TODO
         return False
+
+    def visualize_rewards(self):
+        plt.figure('Reward Visualization')
+        plt.gca().clear()
+        
+        query_vals = np.linspace(start=self.lower_bounds, stop=self.upper_bounds, num=100)
+        r_pred, r_std = izip(*[self.reward_model.predict_reward(x) for x in query_vals])
+        r_pred =  np.asarray(r_pred)
+        r_std = np.asarray(r_std)
+        plt.plot(query_vals, r_pred, 'k-')
+        plt.fill_between(query_vals, r_pred - r_std, r_pred + r_std, alpha=0.5, color='b')
+        x_past, r_past, _ = izip(*self.rounds)
+        plt.plot(x_past, r_past, 'b.')
+        plt.draw()
 
     def execute(self, interface):
         """Begin execution of the specified problem.
@@ -99,13 +118,16 @@ class BayesianOptimizer(object):
             reward, feedback = interface(next_sample)
             self.reward_model.report_sample(x=next_sample, reward=reward)
             self.rounds.append((next_sample, reward, feedback))
+            
+            if self.visualize:
+                self.visualize_rewards()
 
 
 if __name__ == '__main__':
     rospy.init_node('bayesian_optimizer')
 
     interface_info = rospy.get_param('~interface')
-    interface = opt_ros.CritiqueInterface(**interface_info)
+    interface = optim.CritiqueInterface(**interface_info)
 
     # TODO implement progress saving and resuming
     optimizer = BayesianOptimizer()
