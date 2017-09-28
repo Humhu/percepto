f2e68508535363fe4cc97908e81f2000535e03f5
Humphrey Hu <humhu@cmu.edu>
2017-09-28 11:34:55 -0400

    Small bugfixes for CPU reward

--- infitu/scripts/rewards/CPUReward.py
+++ infitu/scripts/rewards/CPUReward.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import time
 import rospy
 import psutil
 from percepto_msgs.msg import RewardStamped
@@ -10,9 +11,19 @@ class CPUReward:
     def __init__( self ):
 
         self.reward_pub = rospy.Publisher( '~reward', RewardStamped, queue_size=0 )
-        self.scale = rospy.get_param( '~scale', 1.0 )
+        self.scale = rospy.get_param( '~scale', psutil.cpu_count() / 100.0 )
         publish_rate = rospy.get_param( '~update_rate' )
-        self.timer = rospy.Timer( rospy.Duration( 1.0/publish_rate ), self.TimerCallback )
+        
+        waitTime = 1.0/publish_rate
+        while not rospy.is_shutdown():
+            msg = RewardStamped()
+
+            msg.header.stamp = rospy.Time.now()
+            msg.reward = -self.scale * psutil.cpu_percent()
+            self.reward_pub.publish( msg );
+            time.sleep(waitTime)
+
+        #self.timer = rospy.Timer( rospy.Duration( 1.0/publish_rate ), self.TimerCallback )
 
     def TimerCallback( self, event ):
         msg = RewardStamped()
