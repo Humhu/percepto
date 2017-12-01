4bfcb45e83d751033023ed6d30e05a75ddd53bd7
Humphrey Hu <humhu@cmu.edu>
2017-11-30 20:17:51 -0500

    Made separate samplers for sars/terms

--- adel/python/adel/dataset.py
+++ adel/python/adel/dataset.py
@@ -2,6 +2,7 @@
 """
 
 import random
+import copy
 import cPickle as pickle
 
 class EpisodeAggregator(object):
@@ -67,12 +68,14 @@ class ValidationHoldout(object):
     def __init__(self, training, holdout, sampler):
         self.training = training
         self.holdout = holdout
-        self.sampler = sampler
+        self.sars_sampler = sampler
+        # TODO Hack?
+        self.term_sampler = copy.copy(sampler)
 
     def report_sars(self, s, a, r, sn):
         """Reports a SARS tuple
         """
-        if self.sampler.sample():
+        if self.sars_sampler.sample():
             self.holdout.report_sars( s, a, r, sn )
         else:
             self.training.report_sars( s, a, r, sn )
@@ -80,7 +83,7 @@ class ValidationHoldout(object):
     def report_terminal(self, s, a):
         """Reports a terminal state-action
         """
-        if self.sampler.sample():
+        if self.term_sampler.sample():
             self.holdout.report_terminal( s, a )
         else:
             self.training.report_terminal( s, a )
